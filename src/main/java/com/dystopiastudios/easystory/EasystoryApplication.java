8ab857f6d37f162c4cc68304587a545f4fbca857
Marco Antonio Manrique Acha <manriqueacham@gmail.com>
2020-09-30 17:28:46 -0500

    Proyecto Base

---
+++ src/main/java/com/dystopiastudios/easystory/EasystoryApplication.java
@@ -0,0 +1,21 @@
+package com.dystopiastudios.easystory;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class EasystoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasystoryApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+}
